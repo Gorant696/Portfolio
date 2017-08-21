cb55eea50aad034f021cd1ac48e7e7a061e3b90f
gorant696 <axelfoley@net.hr>
2017-08-21 21:59:14 +0200

    about

--- includes/aboutcontent.php
+++ includes/aboutcontent.php
@@ -2,18 +2,50 @@
 
     <div class="panel panel-default" style="width: 100% !important; margin: 0 !important;">
 
-        <div class="panel-heading">
-            <h3 class="panel-title"><span style="font-size: 82%;"></span></h3>
-        </div>
+
 
         <div class="panel-body">
 
+            <div class="row">
+                <div class="col-sm-12 col-md-12">
+                    <div class="thumbnail" style="background-color: #66b3ff;">
+                        <br>
+                        <img src="images/profile.jpg" style="border-radius: 70px;">
+                        <div class="caption">
+                            <br>
+                            <p>Hi! My name's Goran and I'm using this site to promote myself in purpose to find a job in web development sector.<br>
+                                I'm trying to improve my skills in developing web applications and sites. I have almost two year experience working with HTML,
+                                CSS, PHP and MySQL programming languages and I'm still learning.<br> I'm responsible, willing to work, and i learn fast.
+                                So, despite the fact that I'm still noob, I really want someone to give me a chance to work on some smaller and simpler
+                                tasks which best suit a beginner like me.<br> I have few projects that I'm still working on, it's something like social media based
+                                on games and movies interests, and some other applications. 
+                                You can visit my social media application on on this URL: mahakali.tk/index.php</p><br>
 
-    
+                            <a href="contact.php"><button class="btn btn-primary" name="submit">Contact me</button></a>
+                        </div>
+                    </div>
+                </div>
+            </div> <!--thumbnail-->
 
+
+            <div class="row">
+                <div class="col-sm-12 col-md-12">
+                    <div class="thumbnail">
+                        <img src="..." alt="...">
+                        <div class="caption">
+                            <h3>Biography</h3>
+
+                            <p><a href="#" class="btn btn-primary" role="button">Download biography</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div> <!--thumbnail-->
 
         </div><!--panel body-->
 
     </div><!--panel-->
 
 </div> <!--flexcontact-->
+
+
+
