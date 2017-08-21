ce47ac18006eeff1d91eee9ccd6ece990f4bb35d
gorant696 <axelfoley@net.hr>
2017-08-21 04:25:59 +0200

    project

---
+++ Projects.php
@@ -0,0 +1,20 @@
+<?php $index = 'Projects'; ?>
+
+<!DOCTYPE html>
+
+<html>
+
+    <?php
+    include 'head.php';
+    ?>
+
+    <body class="bodyindex">
+
+        <?php
+        include 'navbar.php';
+        include 'projectcontent.php';
+        include 'footer.php'; ?>
+        
+    </body>
+
+</html>
---
+++ projectcontent.php
@@ -0,0 +1,76 @@
+<div class="panel panel-default" style="margin: 0px !important;">
+    <div class="panel-heading">
+        <h3 class="panel-title"><a href="https://github.com/Gorant696/GHS" title="Click here to visit GHS on GitHub!" target="_blank">Geek Hot Spot!</a></h3>
+    </div>
+    <div class="panel-body mypanel">
+
+
+
+        <div class="row" style="padding: 5px !important;">
+            <div class="col-xs-12 col-md-12" style="display: flex !important;">
+                <a href="images/background.jpg" class="thumbnail mythumbnail">
+                    <img src="images/background.jpg" alt="...">
+                </a>
+                <a href="images/background.jpg" class="thumbnail mythumbnail" >
+                    <img src="images/background.jpg" alt="...">
+                </a>
+                <a href="images/background.jpg" class="thumbnail mythumbnail">
+                    <img src="images/background.jpg" alt="...">
+                </a>
+                <a href="images/background.jpg" class="thumbnail mythumbnail" >
+                    <img src="images/background.jpg" alt="...">
+                </a>
+                <a href="images/background.jpg" class="thumbnail mythumbnail" >
+                    <img src="images/background.jpg" alt="...">
+                </a>
+            </div>
+
+        </div> <!--thumbnail-->
+
+
+    </div> <!-- panel body-->
+</div> <!-- panel-->
+
+<div class="panel panel-default" style="margin: 0px !important;">
+    <div class="panel-heading">
+        <h3 class="panel-title"><a href="#" title="Click here to visit my portfolio web project on GitHub!" target="_blank">Personal web site and portfolio</a></h3>
+    </div>
+    <div class="panel-body mypanel">
+
+
+
+        <div class="row" style="padding: 5px !important;">
+            <div class="col-xs-12 col-md-12" style="display: flex !important;">
+                <a href="images/background.jpg" class="thumbnail mythumbnail">
+                    <img src="images/background.jpg" alt="...">
+                </a>
+                <a href="images/background.jpg" class="thumbnail mythumbnail" >
+                    <img src="images/background.jpg" alt="...">
+                </a>
+                <a href="images/background.jpg" class="thumbnail mythumbnail">
+                    <img src="images/background.jpg" alt="...">
+                </a>
+                <a href="images/background.jpg" class="thumbnail mythumbnail" >
+                    <img src="images/background.jpg" alt="...">
+                </a>
+                <a href="images/background.jpg" class="thumbnail mythumbnail" >
+                    <img src="images/background.jpg" alt="...">
+                </a>
+            </div>
+
+        </div> <!--thumbnail-->
+
+
+    </div> <!-- panel body-->
+</div> <!-- panel-->
+
+<div class="panel panel-default" style="margin: 0px !important;">
+    <div class="panel-heading">
+        <h3 class="panel-title"><a href="#" title="Click here to visit Office Tools project on GitHub!" target="_blank">Office Tools</a></h3>
+    </div>
+    <div class="panel-body mypanel">
+
+
+    </div> <!-- panel body-->
+</div> <!-- panel-->
+
