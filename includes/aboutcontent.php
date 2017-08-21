d1ec6ee02d56baa2acac3b746609ec57ca1d79f4
gorant696 <axelfoley@net.hr>
2017-08-21 22:53:23 +0200

    svasta

--- includes/aboutcontent.php
+++ includes/aboutcontent.php
@@ -13,13 +13,13 @@
                         <img src="images/profile.jpg" style="border-radius: 70px;">
                         <div class="caption">
                             <br>
-                            <p>Hi! My name's Goran and I'm using this site to promote myself in purpose to find a job in web development sector.<br>
+                            <p>Hi! My name's Goran and I'm using this site to promote myself in purpose to find a job in web development sector.
                                 I'm trying to improve my skills in developing web applications and sites. I have almost two year experience working with HTML,
-                                CSS, PHP and MySQL programming languages and I'm still learning.<br> I'm responsible, willing to work, and i learn fast.
+                                CSS, PHP and MySQL programming languages and I'm still learning. I'm responsible, willing to work, and i learn fast.
                                 So, despite the fact that I'm still noob, I really want someone to give me a chance to work on some smaller and simpler
-                                tasks which best suit a beginner like me.<br> I have few projects that I'm still working on, it's something like social media based
+                                tasks which best suit a beginner like me. I have few projects that I'm still working on, it's something like social media based
                                 on games and movies interests, and some other applications. 
-                                You can visit my social media application on on this URL: mahakali.tk/index.php</p><br>
+                                You can visit my social media application on on this URL: mahakali.tk</p><br>
 
                             <a href="contact.php"><button class="btn btn-primary" name="submit">Contact me</button></a>
                         </div>
@@ -31,11 +31,12 @@
             <div class="row">
                 <div class="col-sm-12 col-md-12">
                     <div class="thumbnail">
-                        <img src="..." alt="...">
+                        <br>
+                        <img src="images/pdf.png" style="width: 150px; height:150px;">
                         <div class="caption">
                             <h3>Biography</h3>
 
-                            <p><a href="#" class="btn btn-primary" role="button">Download biography</a></p>
+                            <p><a href="files/biography.rar" class="btn btn-primary" role="button">Download <br> Biography</a></p>
                         </div>
                     </div>
                 </div>
