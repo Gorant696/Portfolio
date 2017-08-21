419a28f4e888a7a48015b04d3d3b183b0fbd1616
gorant696 <axelfoley@net.hr>
2017-08-21 19:53:11 +0200

    nesto

--- includes/indexcontent.php
+++ includes/indexcontent.php
@@ -1,7 +1,7 @@
-<img class="mySlides indexpiciture" src="images/background.jpg" width="100%" class="indexpicture">
-        <img class="mySlides indexpiciture" src="images/background2.jpg" width="100%" class="indexpicture">
-        <img class="mySlides indexpiciture" src="images/background3.jpg" width="100%" class="indexpicture">
+<img class="mySlides indexpicture" src="images/background.jpg" width="100%">
+<img class="mySlides indexpicture" src="images/background2.jpg" width="100%">
+<img class="mySlides indexpicture" src="images/background3.jpg" width="100%">
 
 
-        <button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
-        <button class="w3-button w3-black w3-display-right" onclick="plusDivs(1)">&#10095;</button>
+<button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
+<button class="w3-button w3-black w3-display-right" onclick="plusDivs(1)">&#10095;</button>
