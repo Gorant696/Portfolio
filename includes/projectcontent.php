f47a79fb1057a651628de302e23f2570b5393cc0
gorant696 <axelfoley@net.hr>
2017-08-31 19:16:33 +0200

    novi projekt

--- includes/projectcontent.php
+++ includes/projectcontent.php
@@ -66,6 +66,39 @@
 
 <div class="panel panel-default" style="margin: 0px !important;">
     <div class="panel-heading">
+        <h3 class="panel-title"><a href="https://github.com/Gorant696/myColorGame" title="Click here to visit myColorGame on GitHub!" target="_blank">myColorGame <span class="fa  fa-github"></span></a></h3>
+    </div>
+    <div class="panel-body mypanel">
+
+
+
+        <div class="row" style="padding: 5px !important;">
+            <div class="col-xs-12 col-md-12" style="display: flex !important;">
+                <a href="images/game1.png" class="thumbnail mythumbnail">
+                    <img src="images/game1.png" alt="...">
+                </a>
+                <a href="images/game2.png" class="thumbnail mythumbnail" >
+                    <img src="images/game2.png" alt="...">
+                </a>
+                <a href="images/game3.png" class="thumbnail mythumbnail">
+                    <img src="images/game3.png" alt="...">
+                </a>
+                <a href="images/game4.png" class="thumbnail mythumbnail" >
+                    <img src="images/game4.png" alt="...">
+                </a>
+                <a href="images/game5.png" class="thumbnail mythumbnail" >
+                    <img src="images/game5.png" alt="...">
+                </a>
+            </div>
+
+        </div> <!--thumbnail-->
+
+
+    </div> <!-- panel body-->
+</div> <!-- panel-->
+
+<div class="panel panel-default" style="margin: 0px !important;">
+    <div class="panel-heading">
         <h3 class="panel-title"><a href="#" title="Click here to visit Office Tools project on GitHub!" target="_blank">Office Tools <span class="fa  fa-github"></span></a></h3>
     </div>
     <div class="panel-body mypanel">
